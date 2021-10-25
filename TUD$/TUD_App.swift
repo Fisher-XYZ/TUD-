2d71994126a0333e6d254ed4087a8b71b6ba6717
Vince F <fzhang@ves.org>
2021-10-25 10:20:49 -0400

    Initial Commit

---
+++ TUD$/TUD_App.swift
@@ -0,0 +1,17 @@
+//
+//  TUD_App.swift
+//  TUD$
+//
+//  Created by fisher on 2021/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TUD_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
